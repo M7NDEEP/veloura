4e304464d7228f295b0783edb17c22370954a74d
Darkhorse27 <mandeep7yadav@gmail.com>
2024-02-26 23:08:23 +0530

    checkoutProgress

---
+++ src/pages/checkout/Dilivery.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Dilivery = () => {
+  return (
+    <div>dilivery</div>
+  )
+}
+
+export default Dilivery
---
+++ src/pages/checkout/Ordersummary.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Ordersummary = () => {
+  return (
+    <div>ordersummary</div>
+  )
+}
+
+export default Ordersummary
